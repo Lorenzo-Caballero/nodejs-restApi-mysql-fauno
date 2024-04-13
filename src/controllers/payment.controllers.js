443368952577fa2eae5908256e26f50469a785c7
Lorenzo Caballero <zeldiuss@gmail.com>
2024-04-13 15:12:36 -0300

    mercadopago integration

--- src/controllers/payment.controllers.js
+++ src/controllers/payment.controllers.js
@@ -1,35 +1,35 @@
-import { MercadoPago, Payment } from "mercadopago";
+import { MercadoPagoConfig, Payment } from 'mercadopago';
 
-// Configuración de MercadoPago
-MercadoPago.configure({
-  access_token: 'TEST-3230960472138078-032423-ab23df1b92b9867ed719267ec3327511-1742793404',
-  // Otras opciones de configuración aquí si es necesario
-});
+// Paso 1: Importar las partes del módulo que deseas usar
+
+// Paso 2: Inicializar el objeto cliente
+const client = new MercadoPagoConfig({ accessToken: 'TEST-3230960472138078-032423-ab23df1b92b9867ed719267ec3327511-1742793404', options: { timeout: 5000, idempotencyKey: 'abc' } });
+
+// Paso 3: Inicializar el objeto de API
+const payment = new Payment(client);
 
 export const createOrder = async (req, res) => {
   try {
-    // Crear una instancia de Payment
-    const payment = new Payment();
-
-    // Crear un objeto de pedido con los detalles del artículo
-    const preference = {
-      items: [
-        {
-          title: "Diseño Tattoo",
-          unit_price: 4000,
-          currency_id: "ARS",
-          quantity: 1,
-        }
-      ]
+    // Paso 4: Crear el objeto de solicitud
+    const body = {
+      transaction_amount: 4000, // Monto de la transacción
+      description: 'Diseño Tattoo', // Descripción del artículo
+      payment_method_id: 'visa', // ID del método de pago (por ejemplo, 'visa', 'mastercard', etc.)
+      payer: {
+        email: 'email@example.com' // Correo electrónico del comprador
+      },
     };
 
-    // Crear la preferencia de pago en MercadoPago
-    const response = await payment.createPreference(preference);
+    // Paso 5: Crear el objeto de opciones de solicitud - Opcional
+    const requestOptions = {
+      idempotencyKey: 'abc', // Clave de idempotencia
+    };
 
-    // Enviar la respuesta de MercadoPago al cliente
-    res.json(response);
+    // Paso 6: Realizar la solicitud
+    const result = await payment.create({ body, requestOptions });
+
+    res.send(result);
   } catch (error) {
-    // Manejar errores
     console.error("Error al crear el pedido:", error);
     res.status(500).json({ error: "Hubo un error al procesar la solicitud." });
   }
