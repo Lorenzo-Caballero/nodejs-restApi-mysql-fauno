d02aa0c5a208f29fcd359c7d69cfc408d10f26dd
Lorenzo Caballero <zeldiuss@gmail.com>
2024-04-13 15:10:35 -0300

    mercadopago integration

--- src/controllers/payment.controllers.js
+++ src/controllers/payment.controllers.js
@@ -1,20 +1,36 @@
-import mercadopago from "mercadopago";
+import { MercadoPago, Payment } from "mercadopago";
+
+// Configuración de MercadoPago
+MercadoPago.configure({
+  access_token: 'TEST-3230960472138078-032423-ab23df1b92b9867ed719267ec3327511-1742793404',
+  // Otras opciones de configuración aquí si es necesario
+});
 
 export const createOrder = async (req, res) => {
-    mercadopago.configure({
-        access_token:
-            "TEST-3230960472138078-032423-ab23df1b92b9867ed719267ec3327511-1742793404",
-    });
+  try {
+    // Crear una instancia de Payment
+    const payment = new Payment();
 
-    const result = await mercadopago.preferences.create({
-        items: [
-            {
-                title: "Diseño Tattoo",
-                unit_price: 4000,
-                currency_id: "ARS",
-                quantity: 1,
-            }
-        ]
-    })
-    res.send(result)
+    // Crear un objeto de pedido con los detalles del artículo
+    const preference = {
+      items: [
+        {
+          title: "Diseño Tattoo",
+          unit_price: 4000,
+          currency_id: "ARS",
+          quantity: 1,
+        }
+      ]
+    };
+
+    // Crear la preferencia de pago en MercadoPago
+    const response = await payment.createPreference(preference);
+
+    // Enviar la respuesta de MercadoPago al cliente
+    res.json(response);
+  } catch (error) {
+    // Manejar errores
+    console.error("Error al crear el pedido:", error);
+    res.status(500).json({ error: "Hubo un error al procesar la solicitud." });
+  }
 };
